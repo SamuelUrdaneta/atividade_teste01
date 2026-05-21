b405be70785ed1a964db7723e6af719ec678f14a
SamuelUrdanetaLugar5 <samuel_urdaneta@>
2026-05-21 09:40:58 -0300

    commit

--- Infra/db/script.sql
+++ Infra/db/script.sql
@@ -3,9 +3,9 @@ CREATE DATABASE sistema_simples;
 USE sistema_simples;
 
 CREATE TABLE users(
-    id INT AUTO_INCREMET PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (username, password) VALUES ('admin', '123456');
+INSERT INTO users (username, password) VALUES ('admin', '123456');
--- indes.php
+++ indes.php
@@ -1,3 +1,32 @@
+<?php
+
+session_start();
+
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db = "sistema_simples";
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+if ($conn->connect_error){
+
+    echo "<script> console.log('erro com a conexão com o banco') </script>";
+
+}else{
+
+    echo "<script> console.log('conexão com o banco foi um sucesso') </script>";
+
+}
+
+
+?>
+
+
+
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,5 +35,13 @@
 </head>
 <body>
     <h1>Tela de Login -PHP</h1>
+    <form action="Post">
+        <label>Usuario</label>
+        <input type="text" name="usuario">
+        <br>
+        <label>Senha</label>
+        <input type="password" name="senha">
+        <br>
+     </form>
 </body>
 </html>
